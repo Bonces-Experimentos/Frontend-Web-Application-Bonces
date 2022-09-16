f42ff69358bc06c25c5aeb5f732b95bce0302c23
Alexander Fausto <alexandermedinacortez@gmail.com>
2022-09-15 21:03:21 -0500

    Add Image

--- src/views/freeViews/AboutUs.vue
+++ src/views/freeViews/AboutUs.vue
@@ -104,38 +104,6 @@
           </v-card>
         </v-col>
       </v-row>
-
-      <v-row class="col-12 mt-5 mb-3">
-        <h1 class="text-center flex">Profesor</h1>
-        <v-col class="col-12">
-          <p class="text-center">
-            Especial agradecimiento al profesor por los conocimientos brindados para la realización de este proyecto.
-          </p>
-        </v-col>
-      </v-row>
-      <v-row class="col-12 justify-center">
-        <v-col class="col-12 col-sm-6 col-md-4">
-          <v-card flat>
-            <v-img src="../../assets/foto-profe.png"></v-img>
-            <v-card-actions>
-              <a
-                  href="https://www.linkedin.com/in/martin-senmache-36115911/"
-                  target="_blank"
-                  class="secondary--text blue-grey lighten-5 flex text-center"
-                  style="width: auto"
-              >
-                <v-avatar rounded>
-                  <v-img
-                      alt=""
-                      src="../../assets/linkedIn.png"
-                  ></v-img>
-                </v-avatar>
-                José Manuel Martín Senmache Sarmiento
-              </a>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
     </v-row>
 
   </v-container>
