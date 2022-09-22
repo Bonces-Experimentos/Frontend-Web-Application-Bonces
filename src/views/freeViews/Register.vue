f5d8dac5aa32e707465da385277521c64bf79f30
Alexander Fausto <alexandermedinacortez@gmail.com>
2022-09-22 14:52:01 -0500

    Validation Register

--- src/views/freeViews/Register.vue
+++ src/views/freeViews/Register.vue
@@ -126,8 +126,9 @@ export default {
         required: v => !!v || "Requerido",
         name: [
          v => !!v || "Requerido",
+         v =>  !/^\s/.test(v)|| 'No ingrese espacios vacio al inicio',
          v => (v && v.length >= 2) || 'Ingrese más de un caracter',
-         v => /^([a-zA-Z])*$/.test(v) || 'Solo ingrese letras'
+         v => /^([a-zA-Z])\s*$/.test(v) || 'Solo ingrese letras'
         ],
         lastName:[
          v => !!v || "Requerido",
